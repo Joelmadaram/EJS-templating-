ac7f37f666a95b9ecc9a03228cb5b271f34f67f0
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-17 16:31:00 -0500

    using json data and looping over to get the response from the objects

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
 
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['blue', 'kity', 'monty', 'winston']
+    res.render('loops', {cats})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } =req.params;
+    const data = redditData[subreddit];
+    console.log(data)
+    res.render('subreddit', { ...data })
 })
 
 app.get('/rand', (req, res) => {
